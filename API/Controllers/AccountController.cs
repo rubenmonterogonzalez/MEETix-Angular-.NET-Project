958692d496ba37cad8d93aacc9064e2e39fdbb29
rubenmonterogonzalez <rubenmontero@rocketmail.com>
2021-06-24 11:18:48 +0200

    End of section 13-14

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -42,7 +42,8 @@ namespace API.Controllers
             {
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
         [HttpPost("login")]
@@ -64,7 +65,8 @@ namespace API.Controllers
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
 
